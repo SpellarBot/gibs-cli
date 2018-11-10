3824285c4f70c14c70c1ffcb1652dac9dbdcd516
Brendan Kennedy <brenwken@gmail.com>
2018-11-09 22:34:39 -0500

    Added sat statistics to readme.md

--- src/calculateStats.js
+++ src/calculateStats.js
@@ -7,32 +7,29 @@ const fsReadFile		= promisify(fs.readFile);
 
 export default async () => {
 	try {
-		let stats		= {};
+		let stats		= { totalImages: 0, sats: {} };
 		let allLinks	= await fsReadFile('links/images.json', { encoding: 'utf-8' });
 		allLinks		= allLinks ? Object.keys(JSON.parse(allLinks)) : {};
 
 		for (const image of allLinks) {
-			let parts = image.split('/');
-
-
-			if (!stats[parts[3]]) {
-				stats[parts[3]] = {
+			let parts	= image.split('/');
+			if (!stats.sats[parts[3]]) {
+				stats.sats[parts[3]] = {
 					images		: 1,
-					startTime	: 0,
-					endTime		: 0
+					startDate	: parts[5],
+					endDate		: parts[5]
 				};
 			} else {
-
-
-				stats[parts[3]] = {
-					images: stats[parts[3]].images + 1
+				stats.sats[parts[3]] = {
+					images		: stats.sats[parts[3]].images + 1,
+					startDate	: stats.sats[parts[3]].startDate < parts[5] ? stats.sats[parts[3]].startDate : parts[5],
+					endDate		: stats.sats[parts[3]].endDate > parts[5] ? stats.sats[parts[3]].endDate : parts[5]
 				};
 			}
-
-			stats.totalImages = stats.totalImages + 1 || 1;
+			stats.totalImages = stats.totalImages + 1;
 		}
 
-		console.log(stats);
+		console.log(JSON.stringify(stats));
 	} catch (error) {
 		console.log('last catch block', error);
 	}
